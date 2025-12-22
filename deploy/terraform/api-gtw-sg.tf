a8f26d39031ae7d5fa12351f122522be96fdb157
Matheus Ribeiro <teteuoliveira12@gmail.com>
2025-12-22 16:55:51 -0300

    feat(infra-microservices): Adicionando JWK para ser compartilhado nos micro servicos

--- deploy/terraform/api-gtw-sg.tf
+++ deploy/terraform/api-gtw-sg.tf
@@ -1,25 +1,25 @@
-# resource "aws_security_group" "vpc_link_sg" {
-#   name        = "${local.project_name}-ecs-vpc-link-sg"
-#   description = "Security Group for API Gateway VPC Link"
-#   vpc_id      = module.vpc.vpc_id
-#
-#   ingress {
-#     description = "Allow HTTP from VPC CIDR"
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = [module.vpc.vpc_cidr_block]
-#   }
-#
-#   egress {
-#     description = "Allow HTTP to ALB"
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = [module.vpc.vpc_cidr_block]
-#   }
-#
-#   tags = {
-#     Name = "${local.project_name}-ecs-vpc-link-sg"
-#   }
-# }
+resource "aws_security_group" "vpc_link_sg" {
+  name        = "${local.project_name}-ecs-vpc-link-sg"
+  description = "Security Group for API Gateway VPC Link"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "Allow HTTP from VPC CIDR"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  egress {
+    description = "Allow HTTP to ALB"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  tags = {
+    Name = "${local.project_name}-ecs-vpc-link-sg"
+  }
+}
--- deploy/terraform/ecs-alb.tf
+++ deploy/terraform/ecs-alb.tf
@@ -1,26 +1,26 @@
-# resource "aws_lb" "ecs_alb" {
-#   name               = md5("${local.project_name}-ecs-internal-alb")
-#   internal           = true
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.alb_sg.id]
-#   subnets            = module.vpc.private_subnets
-#
-#   tags = {
-#     Name = "${local.project_name}-eks-alb"
-#   }
-# }
-#
-# resource "aws_lb_listener" "ecs_alb_listener" {
-#   load_balancer_arn = aws_lb.ecs_alb.arn
-#   port              = 80
-#   protocol          = "HTTP"
-#
-#   default_action {
-#     type = "fixed-response"
-#     fixed_response {
-#       content_type = "text/plain"
-#       message_body = "Default response from ALB"
-#       status_code  = "404"
-#     }
-#   }
-# }
+resource "aws_lb" "ecs_alb" {
+  name               = md5("${local.project_name}-ecs-internal-alb")
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = module.vpc.private_subnets
+
+  tags = {
+    Name = "${local.project_name}-eks-alb"
+  }
+}
+
+resource "aws_lb_listener" "ecs_alb_listener" {
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default response from ALB"
+      status_code  = "404"
+    }
+  }
+}
